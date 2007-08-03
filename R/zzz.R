bf3495318068f1c0b7b5c4f198ece590dca39ef0
Dirk Eddelbuettel <edd@debian.org>
2007-08-03 02:26:16 +0000

    updated/cleaned DESCRIPTION set propset svn:keywords "DATE" on DESCRIPTION move hidden NAMESPACE back in sight and updated accordingly updated zzz.R for NAMESPACE use updated test output

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,11 @@
 # $Id$
-#.onLoad <- function(lib, pkg) {
-#  library.dynam("RDieHarder", pkg, lib )
-#}
-
-.First.lib <- function(lib, pkg) {
+.onLoad <- function(lib, pkg) {
   library.dynam("RDieHarder", pkg, lib )
   .dieharder.generators <<- dieharderGenerators()
 }
+
+#.First.lib <- function(lib, pkg) {
+#  library.dynam("RDieHarder", pkg, lib )
+#  .dieharder.generators <<- dieharderGenerators()
+#}
 
