def6598829ba859c19f528d1d04c3f243a3d69b5
Dirk Eddelbuettel <edd@debian.org>
2018-12-18 08:08:21 -0600

    include libdieharder, adjust build process to use it as fallback

--- src/dieharder.h
+++ src/dieharder.h
@@ -177,7 +177,7 @@ char table_separator;
   *========================================================================
   */
  //int all;               /* Flag to do all tests on selected generator */
- //int binary;            /* Flag to output rands in binary (with -o -f) */
+ unsigned int binary;     /* Flag to output rands in binary (with -o -f) */
  //int bits;              /* bitstring size (in bits) */
  //int diehard;           /* Diehard test number */
  //int generator;         /* GSL generator id number to be tested */
